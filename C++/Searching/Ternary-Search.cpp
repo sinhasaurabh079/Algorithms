4515047bece3f6054e727a0a3a76b368fc84bc08
Saurabh Sinha <sinhasaurabh079@gmail.com>
2023-02-03 20:01:35 +0530

    Update Ternary-Search.cpp

--- C++/Searching/Ternary-Search.cpp
+++ C++/Searching/Ternary-Search.cpp
@@ -35,8 +35,7 @@ class TernarySearch{
 int TernarySearch::Rec_ternarySearch(int l, int r, int key, int ar[])
 {
 	if (r >= l) {
-
-		// Find the mid1 and mid2
+         	// Find the mid1 and mid2
 		int mid1 = l + (r - l) / 3;
 		int mid2 = r - (r - l) / 3;
 
@@ -56,12 +55,10 @@ int TernarySearch::Rec_ternarySearch(int l, int r, int key, int ar[])
 			return Rec_ternarySearch(l, mid1 - 1, key, ar);
 		}
 		else if (key > ar[mid2]) {
-
-			// The key lies in between [mid2,r]
+                       // The key lies in between [mid2,r]
 			return Rec_ternarySearch(mid2 + 1, r, key, ar);
 		}
 		else {
-
 			// The key lies in between [mid1,mid2]
 			return Rec_ternarySearch(mid1 + 1, mid2 - 1, key, ar);
 		}
@@ -77,7 +74,6 @@ int TernarySearch::Rec_ternarySearch(int l, int r, int key, int ar[])
 int TernarySearch::Iter_ternarySearch(int l, int r, int key, int ar[])
 {
     while (r >= l) {
- 
         // Find the mid1 and mid2
         int mid1 = l + (r - l) / 3;
         int mid2 = r - (r - l) / 3;
@@ -101,19 +97,15 @@ int TernarySearch::Iter_ternarySearch(int l, int r, int key, int ar[])
             r = mid1 - 1;
         }
         else if (key > ar[mid2]) {
- 
-            // The key lies in between mid2 and r
+           // The key lies in between mid2 and r
             l = mid2 + 1;
         }
         else {
- 
             // The key lies in between mid1 and mid2
             l = mid1 + 1;
-            r = mid2 - 1;
-        }
+            r = mid2 - 1;}
     }
- 
-    // Key not found
+   // Key not found
     return -1;
 }
 
@@ -122,28 +114,22 @@ int main()
 {
   TernarySearch ts;
 	int l, r, p, key;
-
 	// since ternary Search can only be applied in sorted array
 	int ar[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	// Starting index and ending index
 	l = 0 , r = 9;
 	// Checking for 5 in the array
-	key = 5;
-    
+	key = 5;   
 	// Search the key using ternarySearch - recursive approach
 	p = ts.Rec_ternarySearch(l, r, key, ar);
-
 	// Print the result
 	cout << "Index of " << key<< " is " << p << endl;
-
-
 	// Checking for 50 in the array
 	key = 50;
 	// Search the key using ternarySearch - iterative approach
 	p = ts.Iter_ternarySearch(l, r, key, ar);
 	// Print the result
-	cout << "Index of " << key<< " is " << p << endl;
-		
+	cout << "Index of " << key<< " is " << p << endl;		
     return 0;		
 }
 
